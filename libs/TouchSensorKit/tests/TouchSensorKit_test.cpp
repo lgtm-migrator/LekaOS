09415a61782d64fb900265ee2d3ef6663d84a6f9
Mourad Latoundji <mouradlatoundji@gmail.com>
2022-11-23 12:43:37 +0100

    :recycle: (libs): Refactor Touch Sensor Kit

--- libs/TouchSensorKit/tests/TouchSensorKit_test.cpp
+++ libs/TouchSensorKit/tests/TouchSensorKit_test.cpp
@@ -75,11 +75,6 @@ TEST_F(TouchSensorTest, start)
 	EXPECT_TRUE(touch_sensor_kit.isRunning());
 }
 
-TEST_F(TouchSensorTest, run)
-{
-	touch_sensor_kit.run();
-}
-
 TEST_F(TouchSensorTest, stop)
 {
 	touch_sensor_kit.stop();
